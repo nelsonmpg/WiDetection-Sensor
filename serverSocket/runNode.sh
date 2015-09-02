71a8d2434a087873f4fe1ca6b4c977d7a9caeda4
Nelson Gomes <nelsonmpg@hotmail.com>
2015-09-03 00:21:17 +0100

    Adicionado o git clone ao start do sistema

--- serverSocket/runNode.sh
+++ serverSocket/runNode.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cd /home/linaro/server
+# diretoria do projeto para se fazer o clone
+/home/linaro/WiFinder-Sensor
+git pull https://github.com/ruifgdias/WiFinder-Sensor
+
+# Diretoria para iniciar o servidor http
+cd /home/linaro/WiFinder-Sensor/serverSocket
 node main.js
