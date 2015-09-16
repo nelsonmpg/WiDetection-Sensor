8cb855d79d7605adb21ce94859a81e69f0b54051
Nelson Gomes <nelsonmpg@hotmail.com>
2015-09-16 16:17:51 +0100

    To push.

--- serverSocket/lib/AntDisp.js
+++ serverSocket/lib/AntDisp.js
@@ -12,52 +12,55 @@ var self = this;
  * @returns {undefined}
  */
 module.exports.insertAntDisp = function (valuesHst, client) {
-  r.connect(self.dbData).then(function (conn) {
-    return r.db(self.dbConfig.db).table("AntDisp").get(client).replace(function (row) {
-      return r.branch(
-              row.eq(null),
-              {
-                "nomeAntena": client,
-                "host": [{
+  var pwr = (typeof valuesHst[3] == "undefined") ? "" : valuesHst[3].trim();
+  if ((pwr * 1) != -1 && pwr.trim() != "" && (pwr * 1) < 10 && (pwr * 1) > -140) {
+    r.connect(self.dbData).then(function (conn) {
+      return r.db(self.dbConfig.db).table("AntDisp").get(client).replace(function (row) {
+        return r.branch(
+                row.eq(null),
+                {
+                  "nomeAntena": client,
+                  "host": [{
+                      "macAddress": valuesHst[0],
+                      "data": r.now().inTimezone("+01:00").toEpochTime(),
+                      "Power": pwr,
+                      "BSSID": (typeof valuesHst[5] == "undefined") ? "" : valuesHst[5].replace(/(,| |\r\n|\n|\r)/g, ""),
+                      "nameVendor": r.db("Prefix").table("tblPrefix").get(valuesHst[0].substring(0, 8)).getField("vendor").default("UNKNOWN")
+                    }]
+                },
+        r.branch(
+                row("host")("macAddress").contains(valuesHst[0]),
+                row.merge({
+                  "host": row("host").map(function (d) {
+                    return r.branch(
+                            d("macAddress").eq(valuesHst[0]).default(false),
+                            {
+                              "macAddress": valuesHst[0],
+                              "data": r.now().inTimezone("+01:00").toEpochTime(),
+                              "Power": pwr,
+                              "BSSID": (typeof valuesHst[5] == "undefined") ? "" : valuesHst[5].replace(/(,| |\r\n|\n|\r)/g, ""),
+                              "nameVendor": r.db("Prefix").table("tblPrefix").get(valuesHst[0].substring(0, 8)).getField("vendor").default("UNKNOWN")
+                            },
+                    d);
+                  })}),
+                {
+                  "nomeAntena": client,
+                  "host": row("host").append({
                     "macAddress": valuesHst[0],
                     "data": r.now().inTimezone("+01:00").toEpochTime(),
-                    "Power": (typeof valuesHst[3] == "undefined") ? "" : valuesHst[3].trim(),
+                    "Power": pwr,
                     "BSSID": (typeof valuesHst[5] == "undefined") ? "" : valuesHst[5].replace(/(,| |\r\n|\n|\r)/g, ""),
                     "nameVendor": r.db("Prefix").table("tblPrefix").get(valuesHst[0].substring(0, 8)).getField("vendor").default("UNKNOWN")
-                  }]
-              },
-      r.branch(
-              row("host")("macAddress").contains(valuesHst[0]),
-              row.merge({
-                "host": row("host").map(function (d) {
-                  return r.branch(
-                          d("macAddress").eq(valuesHst[0]).default(false),
-                          {
-                            "macAddress": valuesHst[0],
-                            "data": r.now().inTimezone("+01:00").toEpochTime(),
-                            "Power": (typeof valuesHst[3] == "undefined") ? "" : valuesHst[3].trim(),
-                            "BSSID": (typeof valuesHst[5] == "undefined") ? "" : valuesHst[5].replace(/(,| |\r\n|\n|\r)/g, ""),
-                            "nameVendor": r.db("Prefix").table("tblPrefix").get(valuesHst[0].substring(0, 8)).getField("vendor").default("UNKNOWN")
-                          },
-                  d);
-                })}),
-              {
-                "nomeAntena": client,
-                "host": row("host").append({
-                  "macAddress": valuesHst[0],
-                  "data": r.now().inTimezone("+01:00").toEpochTime(),
-                  "Power": (typeof valuesHst[3] == "undefined") ? "" : valuesHst[3].trim(),
-                  "BSSID": (typeof valuesHst[5] == "undefined") ? "" : valuesHst[5].replace(/(,| |\r\n|\n|\r)/g, ""),
-                  "nameVendor": r.db("Prefix").table("tblPrefix").get(valuesHst[0].substring(0, 8)).getField("vendor").default("UNKNOWN")
-                })}));
-    }, {nonAtomic: true}).run(conn)
-            .finally(function () {
-              conn.close();
-            });
-  }).then(function (output) {
+                  })}));
+      }, {nonAtomic: true}).run(conn)
+              .finally(function () {
+                conn.close();
+              });
+    }).then(function (output) {
 //    console.log("Query output:", output);
-  }).error(function (err) {
-    console.log("***************** Ant Disp **************************");
-    console.log("Failed:", err);
-  });
+    }).error(function (err) {
+      console.log("***************** Ant Disp **************************");
+      console.log("Failed:", err);
+    });
+  }
 };
--- serverSocket/lib/DispMoveis.js
+++ serverSocket/lib/DispMoveis.js
@@ -11,65 +11,68 @@ var self = this;
  * @returns {undefined}
  */
 module.exports.insertDispMovel = function (valsHost, client) {
-  r.connect(self.dbData).then(function (conn) {
-    return r.db(self.dbConfig.db).table("DispMoveis").get(valsHost[0]).replace(function (row) {
-      return r.branch(
-              row.eq(null),
-              {
-                "macAddress": valsHost[0],
-                "nameVendor": r.db("Prefix").table("tblPrefix").get(valsHost[0].substring(0, 8)).getField("vendor").default("UNKNOWN"),
-                "Probed_ESSIDs": (typeof valsHost[6] == "undefined") ? [] : valsHost[6].replace(/(\r\n|\n|\r)/gm, "").split(","),
-                "disp": [{
-                    name: client,
-                    "First_time": r.now().inTimezone("+01:00").toEpochTime(),
-                    "values": [{
-                        "Last_time": r.now().inTimezone("+01:00").toEpochTime(),
-                        "Power": (typeof valsHost[3] == "undefined") ? "" : valsHost[3].trim(),
-                        "BSSID": (typeof valsHost[5] == "undefined") ? "" : valsHost[5].replace(/(,| |\r\n|\n|\r)/g, "")
-                      }]
-                  }]
-              },
-      r.branch(
-              row("disp")("name").contains(client),
-              row.merge({
-                "Probed_ESSIDs": r.db(self.dbConfig.db)
-                        .table("DispMoveis")
-                        .get(valsHost[0])("Probed_ESSIDs")
-                        .setUnion((typeof valsHost[6] == "undefined") ? [] : valsHost[6].replace(/(\r\n|\n|\r)/gm, "").split(",")),
-                "disp": row('disp').map(function (d) {
-                  return r.branch(
-                          d('name').eq(client).default(false),
-                          d.merge({
-                            "values": d('values').append({
-                              "Last_time": r.now().inTimezone("+01:00").toEpochTime(),
-                              "Power": (typeof valsHost[3] == "undefined") ? "" : valsHost[3].trim(),
-                              "BSSID": (typeof valsHost[5] == "undefined") ? "" : valsHost[5].replace(/(,| |\r\n|\n|\r)/g, "")
-                            })}),
-                          d);
-                })}), {
-        "macAddress": valsHost[0],
-        "nameVendor": r.db("Prefix").table("tblPrefix").get(valsHost[0].substring(0, 8)).getField("vendor").default("UNKNOWN"),
-        "Probed_ESSIDs": r.db(self.dbConfig.db)
-                .table("DispMoveis")
-                .get(valsHost[0])("Probed_ESSIDs")
-                .setUnion((typeof valsHost[6] == "undefined") ? [] : valsHost[6].replace(/(\r\n|\n|\r)/gm, "").split(",")),
-        "disp": row("disp").append({
-          "name": client,
-          "First_time": r.now().inTimezone("+01:00").toEpochTime(),
-          "values": [{
-              "Last_time": r.now().inTimezone("+01:00").toEpochTime(),
-              "Power": (typeof valsHost[3] == "undefined") ? "" : valsHost[3].trim(),
-              "BSSID": (typeof valsHost[5] == "undefined") ? "" : valsHost[5].replace(/(,| |\r\n|\n|\r)/g, "")
-            }]})
-      }));
-    }, {nonAtomic: true}).run(conn)
-            .finally(function () {
-              conn.close();
-            });
-  }).then(function (output) {
+  var pwr = (typeof valuesHst[3] == "undefined") ? "" : valuesHst[3].trim();
+  if ((pwr * 1) != -1 && pwr.trim() != "" && (pwr * 1) < 10 && (pwr * 1) > -140) {
+    r.connect(self.dbData).then(function (conn) {
+      return r.db(self.dbConfig.db).table("DispMoveis").get(valsHost[0]).replace(function (row) {
+        return r.branch(
+                row.eq(null),
+                {
+                  "macAddress": valsHost[0],
+                  "nameVendor": r.db("Prefix").table("tblPrefix").get(valsHost[0].substring(0, 8)).getField("vendor").default("UNKNOWN"),
+                  "Probed_ESSIDs": (typeof valsHost[6] == "undefined") ? [] : valsHost[6].replace(/(\r\n|\n|\r)/gm, "").split(","),
+                  "disp": [{
+                      name: client,
+                      "First_time": r.now().inTimezone("+01:00").toEpochTime(),
+                      "values": [{
+                          "Last_time": r.now().inTimezone("+01:00").toEpochTime(),
+                          "Power": pwr,
+                          "BSSID": (typeof valsHost[5] == "undefined") ? "" : valsHost[5].replace(/(,| |\r\n|\n|\r)/g, "")
+                        }]
+                    }]
+                },
+        r.branch(
+                row("disp")("name").contains(client),
+                row.merge({
+                  "Probed_ESSIDs": r.db(self.dbConfig.db)
+                          .table("DispMoveis")
+                          .get(valsHost[0])("Probed_ESSIDs")
+                          .setUnion((typeof valsHost[6] == "undefined") ? [] : valsHost[6].replace(/(\r\n|\n|\r)/gm, "").split(",")),
+                  "disp": row('disp').map(function (d) {
+                    return r.branch(
+                            d('name').eq(client).default(false),
+                            d.merge({
+                              "values": d('values').append({
+                                "Last_time": r.now().inTimezone("+01:00").toEpochTime(),
+                                "Power": pwr,
+                                "BSSID": (typeof valsHost[5] == "undefined") ? "" : valsHost[5].replace(/(,| |\r\n|\n|\r)/g, "")
+                              })}),
+                            d);
+                  })}), {
+          "macAddress": valsHost[0],
+          "nameVendor": r.db("Prefix").table("tblPrefix").get(valsHost[0].substring(0, 8)).getField("vendor").default("UNKNOWN"),
+          "Probed_ESSIDs": r.db(self.dbConfig.db)
+                  .table("DispMoveis")
+                  .get(valsHost[0])("Probed_ESSIDs")
+                  .setUnion((typeof valsHost[6] == "undefined") ? [] : valsHost[6].replace(/(\r\n|\n|\r)/gm, "").split(",")),
+          "disp": row("disp").append({
+            "name": client,
+            "First_time": r.now().inTimezone("+01:00").toEpochTime(),
+            "values": [{
+                "Last_time": r.now().inTimezone("+01:00").toEpochTime(),
+                "Power": pwr,
+                "BSSID": (typeof valsHost[5] == "undefined") ? "" : valsHost[5].replace(/(,| |\r\n|\n|\r)/g, "")
+              }]})
+        }));
+      }, {nonAtomic: true}).run(conn)
+              .finally(function () {
+                conn.close();
+              });
+    }).then(function (output) {
 //    console.log("Query Disp output:\n", output);
-  }).error(function (err) {
-    console.log("***************** Dispp Moveis **************************");
-    console.log("Failed:", err);
-  });
+    }).error(function (err) {
+      console.log("***************** Dispp Moveis **************************");
+      console.log("Failed:", err);
+    });
+  }
 };
