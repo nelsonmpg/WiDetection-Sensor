2e903254a6eca126c54ddc8170e326a62f7937ec
Nelson Gomes <nelsonmpg@hotmail.com>
2015-09-23 16:58:45 +0100

    lmk

--- serverSocket/runAirmon.c
+++ serverSocket/runAirmon.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
 	system(command1);
 	
 	sleep(3);
-	snprintf(command2, sizeof(command2), "sudo nohup airodump-ng -w %s/scanNetworks --output-format csv --write-interval 5 --ignore-negative-one wlan0mon > /dev/null 2>&1", argv[1]);
+	snprintf(command2, sizeof(command2), "sudo nohup airodump-ng -w %s/scanNetworks --output-format csv --write-interval 5 wlan0mon > /dev/null 2>&1", argv[1]);
 	// execute command Run Airmon
 	system(command2);
 	
