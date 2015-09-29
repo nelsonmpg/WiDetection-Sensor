062a54783dfab380f17dd547d9b494142ff0e1d0
Nelson Gomes <nelsonmpg@hotmail.com>
2015-09-29 14:13:38 +0100

    run tunnel

---
+++ serverSocket/runTunneling.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# verifica se  o tunnel existe se não executa o script para o criar
+if [[ $? -ne 0 ]]; then
+	sudo killall ssh
+	cd /home/linaro/WiFinder-Sensor/serverSocket
+  .	/expect.exp `cat ConfigSKT.ini | grep "sshaccess" | cut -d" " -f3`
+fi
