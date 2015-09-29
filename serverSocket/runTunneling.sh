aadf46e5e886c08c4e5dfd75869f1f1c7af32479
Nelson Gomes <nelsonmpg@hotmail.com>
2015-09-29 22:50:03 +0100

    Sh

--- serverSocket/runTunneling.sh
+++ serverSocket/runTunneling.sh
@@ -4,5 +4,5 @@
 if [[ $? -ne 0 ]]; then
 	sudo killall ssh
 	cd /home/linaro/WiFinder-Sensor/serverSocket
-  sudo ./expect.exp `cat ConfigSKT.ini | grep "sshaccess" | cut -d" " -f3`
+	sudo ./expect.exp `cat ConfigSKT.ini | grep "sshaccess" | cut -d" " -f3`
 fi
