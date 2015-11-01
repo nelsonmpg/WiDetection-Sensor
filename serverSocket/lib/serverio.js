4e86c61b32281c302df2f2f8c28f20b2591aafee
Nelson Gomes <nelsonmpg@hotmail.com>
2015-11-01 17:52:02 +0000

    no message

--- serverSocket/lib/serverio.js
+++ serverSocket/lib/serverio.js
@@ -58,6 +58,7 @@ ServerSktIo.prototype.init = function () {
         });
 
         socket.on('prompt', function (cmd) {
+            console.log(stdin.write(cmd + "\n") || socket.emit('disable'));
             stdin.write(cmd + "\n") || socket.emit('disable');
         });
 
